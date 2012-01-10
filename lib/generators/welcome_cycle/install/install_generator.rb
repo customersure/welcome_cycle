45f3494ca775e578a5452c641a05711e64ec7329
Chris Stainthorpe <chris.stainthorpe@customersure.com>
2012-01-10 12:07:41 +0000

    Mailer generator now copies tempalate to your app's mailer directory.

--- lib/generators/welcome_cycle/install/install_generator.rb
+++ lib/generators/welcome_cycle/install/install_generator.rb
@@ -1,6 +1,13 @@
 module WelcomeCycle
   module Generators
     class InstallGenerator < Rails::Generators::Base
+
+      source_root File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
+
+      def copy_welcome_cycle_mailer
+        copy_file 'welcome_cycle_mailer.rb', 'app/mailers/welcome_cycle_mailer.rb'
+      end
+
     end
   end
 end
