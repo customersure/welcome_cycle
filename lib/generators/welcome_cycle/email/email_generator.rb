5ff389380d151ec76f16354868a909256ed5ad01
Chris Stainthorpe <chris.stainthorpe@customersure.com>
2012-01-10 14:57:31 +0000

    Generator for new welcome_cycle emails.

---
+++ lib/generators/welcome_cycle/email/email_generator.rb
@@ -0,0 +1,39 @@
+module WelcomeCycle
+  module Generators
+    class EmailGenerator < Rails::Generators::NamedBase
+
+      source_root File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
+
+      def register_new_mail
+        append_to_file 'lib/welcome_cycle.rb' do
+          <<-EOS
+
+
+WelcomeCycle::Email.new("#{file_name.gsub(/_/, ' ').capitalize}") do
+  days 1 # on which days of the trial should the mail be sent?
+  scope do
+    # send if these Arel conditions are met
+  end
+end
+          EOS
+        end
+      end
+
+      def add_new_mailer_action
+        gsub_file 'app/mailers/welcome_cycle_mailer.rb', '# your actions here', ''
+        inject_into_class 'app/mailers/welcome_cycle_mailer.rb', WelcomeCycleMailer do
+          <<-EOS
+
+    def #{file_name}
+    end
+EOS
+        end
+      end
+
+      def add_new_mailer_templates
+        create_file "app/views/welcome_cycle_mailer/#{file_name}.text.erb"
+        create_file "app/views/welcome_cycle_mailer/#{file_name}.html.erb"
+      end
+    end
+  end
+end
