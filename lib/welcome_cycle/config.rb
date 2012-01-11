b205f1b80f866846bb5d5f1b3ab300fa8915507a
Luke Brown <tsdbrown@gmail.com>
2012-01-11 15:06:13 +0000

    Changed default config to Subscription

--- lib/welcome_cycle/config.rb
+++ lib/welcome_cycle/config.rb
@@ -4,8 +4,8 @@ module WelcomeCycle
     attr_accessor :base_class, :welcome_cycle_start_date, :welcome_cycle_end_date
 
     def initialize
-      @base_class = Organisation if defined?(Organisation)
-      @welcome_cycle_start_date = :created_at
+      @base_class = Subscription if defined?(Subscription)
+      @welcome_cycle_start_date = :trial_stared_at
       @welcome_cycle_end_date = :trial_ends_at
     end
   end
