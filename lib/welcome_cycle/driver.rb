abea242d1af8cddfe1959e59ba7ecca44d108263
Luke Brown <tsdbrown@gmail.com>
2012-01-13 17:09:21 +0000

    Added checks to ensure WelcomeCycle is configured before the Driver is set to run

--- lib/welcome_cycle/driver.rb
+++ lib/welcome_cycle/driver.rb
@@ -1,7 +1,8 @@
 module WelcomeCycle
   class Driver
     def self.run
-      # TODO: Need to check we have a base_class
+      raise 'WelcomeCycle is not configured! See the README config section for help.' if WelcomeCycle.config.nil?
+      raise "You must set the 'base_class' option. See README config section." if WelcomeCycle.config.base_class.nil?
       WelcomeCycle::EmailRegister.instance.emails.each { |e| e.send_to_recipients! }
     end
   end
--- spec/welcome_cycle/driver_spec.rb
+++ spec/welcome_cycle/driver_spec.rb
@@ -6,16 +6,33 @@ describe WelcomeCycle::Driver do
     let(:email1) { mock 'email 1' }
     let(:email2) { mock 'email 1' }
 
-    before do
-      WelcomeCycle::EmailRegister.instance << email1
-      WelcomeCycle::EmailRegister.instance << email2
+    context "when WelcomeCycle has not been configured" do
+      it 'raises an error telling you to configure it' do
+        lambda { WelcomeCycle::Driver.run }.should raise_error('WelcomeCycle is not configured! See the README config section for help.')
+      end
     end
 
-    it 'calls #send_to_recipients! on each email in the register' do
-      email1.should_receive(:send_to_recipients!)
-      email2.should_receive(:send_to_recipients!)
-      WelcomeCycle::Driver.run
+    context "when no base_class is configured" do
+      before { WelcomeCycle.configure {} }
+      it 'raises an error telling you to set the base class' do
+        lambda { WelcomeCycle::Driver.run }.should raise_error("You must set the 'base_class' option. See README config section.")
+      end
     end
+
+    context "when configured with a base class" do
+      before do
+        WelcomeCycle.configure { |c| c.base_class = mock('class') }
+        WelcomeCycle::EmailRegister.instance << email1
+        WelcomeCycle::EmailRegister.instance << email2
+      end
+
+      it 'calls #send_to_recipients! on each email in the register' do
+        email1.should_receive(:send_to_recipients!)
+        email2.should_receive(:send_to_recipients!)
+        WelcomeCycle::Driver.run
+      end
+    end
+
   end
 
 end
